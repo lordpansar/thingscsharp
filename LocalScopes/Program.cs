4c2089ef23729a81dcb3544737e2725559afbe48
Magnus Sundström <kmvsundstrom@gmail.com>
2021-02-19 15:04:33 +0100

    Added local scopes example

---
+++ LocalScopes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LocalScopes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int outside = 1;
+            
+            {
+                int inside = outside;
+                inside = 2;
+            }
+
+            outside = inside;
+        }
+    }
+}
