e44adf2db92500f7a66f01ef30856ac582424136
Magnus Sundström <kmvsundstrom@gmail.com>
2021-02-23 16:14:05 +0100

    Added number separators example project

---
+++ BigNumberSeparators/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BigNumberSeparators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add underscores to big numbers to increase readability. The compiler will ignore them.
+            int myInt = 2_146_654_442;
+            long myLong = 4_000_000_000;
+            short myShort = 1_2_3;
+            double mydouble = 20_123_443.9;
+            float myFloat = 12_313.23f;
+            decimal myDecimal = 1.126_549_878_945_641m;
+
+
+            Console.WriteLine($"Your number is {myInt}");
+            Console.ReadLine();
+        }
+    }
+}
