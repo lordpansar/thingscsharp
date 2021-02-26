c4de99138059b148f83903bffdc6355737d7d76f
Magnus Sundström <kmvsundstrom@gmail.com>
2021-02-26 11:30:55 +0100

    Added optional parameters example project

---
+++ OptionalParameters/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //By setting one or more default values in the method signature, you can call the method without passing all required data. 
+            PrintPersonInfo("Alex", 32, "Software developer");
+            PrintPersonInfo("Kim", 29);
+            Console.ReadLine();
+        }
+
+        public static void PrintPersonInfo(string name, int age, string occupation = "Unemployed")
+        {
+            Console.WriteLine($"Name: {name} Age: {age} Occupation: {occupation}");
+        }
+    }
+}
