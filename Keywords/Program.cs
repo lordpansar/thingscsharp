45ed8d0f7ba2cecdd1b9f14e3e69cfe72bbab28d
Magnus Sundström <kmvsundstrom@gmail.com>
2021-02-26 11:41:30 +0100

    Added keywords example project

---
+++ Keywords/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Keywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adding an @ before the variable name allows you to use the normally reserved keywords in C# 
+            var @double = 1.1;
+            var @string = "All your base are belong to us";
+
+            
+            Console.WriteLine($"Your number is: {@double}");
+            Console.WriteLine(@string);
+
+            Console.ReadLine();
+        }
+    }
+}
