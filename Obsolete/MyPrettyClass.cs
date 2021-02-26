4a008af54c4ca74295198a7232b7c96519404ad0
Magnus Sundström <kmvsundstrom@gmail.com>
2021-02-26 12:00:04 +0100

    Added obsolete attribute example project

---
+++ Obsolete/MyPrettyClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Obsolete
+{
+    //It also works on class level!
+    [Obsolete("Uh, this class is so 2019")]
+    public class MyPrettyClass
+    {
+        public void DoSomething()
+        {
+            //Do something
+        }
+    }
+}
---
+++ Obsolete/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Obsolete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adding the Obsolete attribute to a method allows you to give information about the method's current status.
+            //There is also an overload that allows you to set whether the attribute should mark usage of the method as an error or just as a warning.
+            MyObsoleteMethod();
+            MyNewMethod();
+
+            //It also works on class level!
+            var myPrettyObject = new MyPrettyClass();
+
+            Console.ReadLine();
+        }
+
+        [Obsolete("This endpoint is nearing end of life. Consider upgrading to v2.0 of our API", false)]
+        public static void MyObsoleteMethod()
+        {
+            Console.WriteLine("I'm old and broken!");
+        }
+
+        public static void MyNewMethod()
+        {
+            Console.WriteLine("Brand spanking new!");
+        }
+    }
+}
