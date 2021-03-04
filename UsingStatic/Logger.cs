bce4a86e759025777441244ac1e13145086c3648
Magnus Sundström <kmvsundstrom@gmail.com>
2021-03-04 16:54:50 +0100

    Adding using static example project

---
+++ UsingStatic/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsingStatic
+{
+    public static class Logger
+    {
+        public static void Log(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
---
+++ UsingStatic/Program.cs
@@ -0,0 +1,26 @@
+﻿using static System.Console; //Referencing static types
+using static UsingStatic.Logger;
+
+namespace UsingStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isSuccess = true;
+
+            if(isSuccess)
+            {
+                Log("System is  a-okay!"); //Using a static member without referencing it's type
+            }
+            else
+            {
+                Log("The system is down :(");
+            }
+
+            WriteLine("All your base are belong to us"); //Also works with predefined static types
+
+            ReadLine();
+        }
+    }
+}
